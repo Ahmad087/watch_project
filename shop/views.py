a1dfacc8eaa4305186f8ecb0648c0a9a973dbe8c
Ahmad087 <43409056+Ahmad087@users.noreply.github.com>
2019-12-02 20:31:10 +0000

    alot of changes made

---
+++ shop/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Category, Product
+from django.db.models import Count
+from django.contrib.auth.models import Group, User
+from .forms import SignUpForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+def signinView(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('products')
+            else:
+                return redirect('signup')
+    else:
+        form = AuthenticationForm()
+    return render(request,'accounts/signin.html', {'form':form})
+
+def signoutView(request):
+    logout(request)
+    return redirect('signin')
+
+def signupView(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            signup_user = User.objects.get(username=username)
+            customer_group = Group.objects.get(name='Customer')
+            customer_group.user_set.add(signup_user)
+    else:
+        form = SignUpForm()
+    return render(request, 'accounts/signup.html', {'form':form})
+
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(request,
+                  'shop/product/list.html',
+                  {'category': category,
+                   'categories': categories,
+                   'products': products})
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product,
+                                id=id,
+                                slug=slug,
+                                available=True)
+    return render(request,
+                  'shop/product/detail.html',
+                  {'product': product})
